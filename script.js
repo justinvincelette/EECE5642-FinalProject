aecce48eea5261cb996527f8029f5305e60addac
justinvincelette <justinvincelette@gmail.com>
2018-03-25 16:46:50 -0400

    Add poverty csv data

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ $(document).ready(function() {
       .attr('width', width)
       .attr('height', height);
 
-  d3.json('us-states.json', function(json) {
-    svg.selectAll('path')
-        .data(json.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .style('stroke', '#fff')
-        .style('stroke-width', '1')
-        .style('fill', '#000');
+  d3.csv("povertydata.csv", function(data) {
+    d3.json('us-states.json', function(json) {
+      svg.selectAll('path')
+          .data(json.features)
+          .enter()
+          .append('path')
+          .attr('d', path)
+          .style('stroke', '#fff')
+          .style('stroke-width', '1')
+          .style('fill', '#000');
+    });
   });
 
 });
