afa8fbd87b98704fa4273c731766d9c3f3e1e86d
justinvincelette <justinvincelette@gmail.com>
2018-03-25 17:51:09 -0400

    States have color scale representing the poverty change from 1980 to 2016

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var values, povertyChanges, min, max, colorScale;
+
   var width = 960;
   var height = 500;
 
@@ -12,6 +14,11 @@ $(document).ready(function() {
 
   d3.csv("povertydata.csv", function(data) {
     d3.json('us-states.json', function(json) {
+      values = calculatePovertyChanges(data, 1980, 2016);
+      povertyChanges = values[0];
+      min = values[1];
+      max = values[2];
+      colorScale = d3.scale.linear().domain([min, max]).range(['red', 'green']);
       svg.selectAll('path')
           .data(json.features)
           .enter()
@@ -19,8 +26,28 @@ $(document).ready(function() {
           .attr('d', path)
           .style('stroke', '#fff')
           .style('stroke-width', '1')
-          .style('fill', '#000');
+          .style('fill', function(d) {
+            return colorScale(povertyChanges[d.properties.name]);
+          });
     });
   });
 
 });
+
+function calculatePovertyChanges(data, start, end) {
+  var min = 100;
+  var max = 0;
+  var result = {};
+  for (var i = 0; i < data.length; i++) {
+    var state = data[i].state;
+    var diff = data[i][start] - data[i][end];
+    result[state] = data[i][start] - data[i][end];
+    if (diff < min) {
+      min = diff;
+    }
+    if (diff > max) {
+      max = diff;
+    }
+  }
+  return [result, min, max];
+}
